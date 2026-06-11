4a98c2119d432910e7504186784258619d9b7af4
Danish Siddiqui <danish.09.siddiqui@gmail.com>
2026-06-11 16:42:25 +0100

    updates app.py

--- app.py
+++ app.py
@@ -1,1 +1,16 @@
-print("Hello, this Python app is running inside Docker!")
+#print("Hello, this Python app is running inside Docker!")
+
+from flask import Flask
+import os
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hello NCC"
+
+if __name__ == "__main__":
+    app.run(
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 10000))
+    )
